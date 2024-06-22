ebc41aea6668e5755859aa7273cf71ce22897a4b
kir925 <kriper.men2015@mail.ru>
2024-06-23 02:10:36 +0800

    Update simulate_and_publish.py

--- scripts/simulate_and_publish.py
+++ scripts/simulate_and_publish.py
@@ -1,1 +1,28 @@
+import time
+import paho.mqtt.client as mqtt_client
+import random
+
+broker = "broker.emqx.io"
+client_id = 'publisher'
+
+def publish_data(topic, data):
+    client = mqtt_client.Client(client_id)
+    client.connect(broker)
+    client.loop_start()
+
+    client.publish(topic, data)
+
+    client.disconnect()
+    client.loop_stop()
+
+def simulate_and_publish(satellite):
+    while True:
+        state = "on" if random.randint(0, 1) == 0 else "off"
+        state += satellite
+        publish_data("lab/leds/state", state)
+        time.sleep(2)
+
+if __name__ == "__main__":
+    satellite = "ANTC00CHL"  # Здесь можно сделать параметр для передачи из FastAPI
+    simulate_and_publish(satellite)
 
