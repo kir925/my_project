1f1393aaf69ca66fcc4898dc3d24e07b7f2321ac
kir925 <kriper.men2015@mail.ru>
2024-06-23 01:51:14 +0800

    Update publisher.py

--- app/publisher.py
+++ app/publisher.py
@@ -1,1 +1,27 @@
+import time
+import paho.mqtt.client as mqtt_client
+from gnss_tec import rnx
+
+broker = "broker.emqx.io"
+topic = "gnss/data"
+
+def start_publishing(satellite):
+    def on_connect(client, userdata, flags, rc):
+        print("Connected with result code " + str(rc))
+    
+    client = mqtt_client.Client()
+    client.on_connect = on_connect
+    client.connect(broker)
+    client.loop_start()
+
+    with open(f'data/{satellite}_R_20241680000_01D_30S_MO.rnx') as obs_file:
+        reader = rnx(obs_file)
+        for tec in reader:
+            message = f'{tec.timestamp} {tec.satellite}: {tec.phase_tec} {tec.p_range_tec}'
+            print(f"Publishing: {message}")
+            client.publish(topic, message)
+            time.sleep(2)  # Задержка между сообщениями
+
+    client.loop_stop()
+    client.disconnect()
 
