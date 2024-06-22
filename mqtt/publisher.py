616aea5196aad625c15a427dc9479e8c6c9d8126
kir925 <kriper.men2015@mail.ru>
2024-06-23 02:10:48 +0800

    Update publisher.py

--- mqtt/publisher.py
+++ mqtt/publisher.py
@@ -1,1 +1,21 @@
+import time
+import paho.mqtt.client as mqtt_client
+
+broker = "broker.emqx.io"
+client_id = 'subscriber'
+
+def on_message(client, userdata, message):
+    data = message.payload.decode("utf-8")
+    print("Received message:", data)
+
+def subscribe_to_mqtt():
+    client = mqtt_client.Client(client_id)
+    client.on_message = on_message
+
+    client.connect(broker)
+    client.subscribe("lab/leds/state")
+    client.loop_forever()
+
+if __name__ == "__main__":
+    subscribe_to_mqtt()
 
