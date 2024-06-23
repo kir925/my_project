cca93cea5c255786df17ad205d4928b592b680b4
kir925 <kriper.men2015@mail.ru>
2024-06-23 16:56:09 +0800

    Update main.py

--- fastapi/main.py
+++ fastapi/main.py
@@ -1,1 +1,10 @@
+from fastapi import FastAPI
+import subprocess
+
+app = FastAPI()
+
+@app.post("/request-data/")
+async def request_data(satellite_name: str):
+    subprocess.Popen(["python", "/app/data_downloader/uploading_files.py", satellite_name])
+    return {"message": "Processing started"}
 
