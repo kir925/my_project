6345f4a634c7f75bc1c2ed23bf397490ade042e3
kir925 <kriper.men2015@mail.ru>
2024-06-25 14:45:13 +0800

    Update user_client.py

--- users/user_client.py
+++ users/user_client.py
@@ -1,1 +1,16 @@
+import requests
+
+def main():
+    server = "http://127.0.0.1:8000"
+    topics = ["lab/gnss/DAEJ00KOR_R_20240010000_01D_30S_MO.rnx"]
+
+    for topic in topics:
+        response = requests.get(f"{server}/subscribe/{topic}")
+        if response.status_code == 200:
+            print(f"Subscribed to {topic}")
+        else:
+            print(f"Failed to subscribe to {topic}")
+
+if __name__ == "__main__":
+    main()
 
