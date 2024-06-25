091c93636189e28aa7325cd777bcad62fdd69c3c
kir925 <kriper.men2015@mail.ru>
2024-06-25 17:54:32 +0800

    Update publisher.py

--- data_emulation/publisher.py
+++ data_emulation/publisher.py
@@ -1,31 +1,23 @@
 import time
 import paho.mqtt.client as mqtt_client
-from gnss_tec import rnx
-import os
-import sys
+from gnss_tec import rnx  # Подставьте свой модуль или библиотеку для чтения rnx файлов
 
 broker = "broker.emqx.io"
-topic_prefix = "lab/gnss/"
-interval = 30  # Периодичность публикации данных
+topic = "lab/gnss/data"
+
+client = mqtt_client.Client('gnss_publisher')
+client.connect(broker)
+client.loop_start()
 
 def publish_data(file_path):
-    client = mqtt_client.Client(f'publisher_{os.path.basename(file_path)}')
-    client.connect(broker)
-    client.loop_start()
-
-    topic = f"{topic_prefix}{os.path.basename(file_path)}"
-    
     with open(file_path) as obs_file:
         reader = rnx(obs_file)
         for tec in reader:
             message = f"{tec.timestamp} {tec.satellite} {tec.phase_tec} {tec.p_range_tec}"
             client.publish(topic, message)
-            time.sleep(interval)  # Публикация каждые 30 секунд
-
-    client.loop_stop()
-    client.disconnect()
+            time.sleep(30)  # публикуем каждые 30 секунд
 
 if __name__ == "__main__":
-    file_path = sys.argv[1]
-    publish_data(file_path)
-
+    publish_data('example.rnx')  # Пример файла .rnx для публикации
+    client.loop_stop()
+    client.disconnect()
