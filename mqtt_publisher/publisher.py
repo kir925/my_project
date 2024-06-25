73e805b750b585b0ff54446a1c6e76cf31b0700f
kir925 <kriper.men2015@mail.ru>
2024-06-25 21:03:28 +0800

    Update publisher.py

--- mqtt_publisher/publisher.py
+++ mqtt_publisher/publisher.py
@@ -1,1 +1,20 @@
+import time
+import paho.mqtt.client as mqtt_client
+import random
+
+broker = "broker.emqx.io"
+client = mqtt_client.Client('publisher_client_id')
+
+def publish_data(data):
+    client.connect(broker)
+    client.publish("gnss_data", data)
+    client.disconnect()
+
+if __name__ == "__main__":
+    client.connect(broker)
+    client.loop_start()
+    for data in read_rinex_file("rinex_reader/rinex_files/ASCG00SHN_R_20240010000_01D_30S_MO.rnx"):
+        publish_data(data)
+        time.sleep(30)  # Publish data every 30 seconds
+    client.loop_stop()
 
