a865cef1ca51735f3d73f8424e5d4a248c3258a9
kir925 <kriper.men2015@mail.ru>
2024-06-25 14:43:12 +0800

    Update process_data.py

--- data_processing/process_data.py
+++ data_processing/process_data.py
@@ -1,1 +1,13 @@
+import os
+import subprocess
+
+def process_files(directory):
+    for filename in os.listdir(directory):
+        if filename.endswith(".crx"):
+            crx_file = os.path.join(directory, filename)
+            rnx_file = crx_file.replace(".crx", ".rnx")
+            subprocess.run(["crx2rnx", crx_file, rnx_file])
+
+if __name__ == "__main__":
+    process_files('/path/to/unzipped/files')
 
