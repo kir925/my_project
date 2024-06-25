78193caef8c37af87f1c62b2bb01ba6154e058aa
kir925 <kriper.men2015@mail.ru>
2024-06-25 21:02:58 +0800

    Update main.py

--- fastapi_server/main.py
+++ fastapi_server/main.py
@@ -1,1 +1,13 @@
+from fastapi import FastAPI, Query
+from rinex_reader.rinex_reader import read_rinex_file
+from typing import List
+
+app = FastAPI()
+
+@app.get("/subscribe/")
+async def subscribe(satellite_name: List[str] = Query(None)):
+    for sat_name in satellite_name:
+        filename = f"{sat_name}_R_20240010000_01D_30S_MO.rnx"
+        for data in read_rinex_file(f"rinex_reader/rinex_files/{filename}"):
+            yield data
 
