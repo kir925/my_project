2f21288f2a751926b6b31571cdf74ec8ae0b8834
kir925 <kriper.men2015@mail.ru>
2024-06-25 17:00:57 +0800

    Update download_data.py

--- data_processing/download_data.py
+++ data_processing/download_data.py
@@ -1,9 +1,13 @@
+import schedule
+import time
 import requests
-import sys
+import os
+from datetime import datetime, timedelta
 
-def download_data(date):
+def download_data(date, save_dir):
+    os.makedirs(save_dir, exist_ok=True)
     link = f"https://api.simurg.space/datafiles/map_files?date={date}"
-    file_name = f"{date}.zip"
+    file_name = os.path.join(save_dir, f"{date}.zip")
     with open(file_name, "wb") as f:
         print("Downloading %s" % file_name)
         response = requests.get(link, stream=True)
@@ -21,3 +25,18 @@ def download_data(date):
                 sys.stdout.write("\r[%s%s]" % ('=' * done, ' ' * (50-done)))
                 sys.stdout.flush()
 
+def job():
+    # Вычисляем дату 5 дней назад от текущего момента времени
+    date_5_days_ago = datetime.now() - timedelta(days=5)
+    date = date_5_days_ago.strftime("%Y-%m-%d")
+    save_dir = 'downloads'
+
+    download_data(date, save_dir)
+
+if __name__ == "__main__":
+    # Запускаем задачу каждый день в 22:00
+    schedule.every().day.at("22:00").do(job)
+
+    while True:
+        schedule.run_pending()
+        time.sleep(60)  # Проверяем расписание каждую минуту
